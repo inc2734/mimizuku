5e2cf68a74442b09a8dd40f5e23f58dc9a408795
Takashi Kitajima <inc@2inc.org>
2016-07-17 14:39:39 +0900

    Add singular.php #3

--- single.php
+++
@@ -1,6 +0,0 @@
-<?php
-use Mimizuku\Controller as Controller;
-
-Controller::set_layout( 'layout/right-sidebar' );
-Controller::set_view( 'template-parts/content/content', 'single' );
-Controller::render();
--- singular.php
+++ singular.php
@@ -2,5 +2,5 @@
 use Mimizuku\Controller as Controller;
 
 Controller::set_layout( 'layout/right-sidebar' );
-Controller::set_view( 'template-parts/content/content', 'page' );
+Controller::set_view( 'template-parts/content/content', get_post_type() );
 Controller::render();
