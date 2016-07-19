a27663dda53ca5b757000291168f04b1f5fea201
Takashi Kitajima <inc@2inc.org>
2016-07-19 11:18:09 +0900

    Remove footer-nav

--- functions/setup-nav-menus.php
+++ functions/setup-nav-menus.php
@@ -10,7 +10,6 @@ function setup_nav_menus() {
 	register_nav_menus( [
 		'global-nav' => esc_html__( 'Global Navigation (For PC)', 'mimizuku' ),
 		'drawer-nav' => esc_html__( 'Drawer Navigation (For Mobile)', 'mimizuku' ),
-		'footer-nav' => esc_html__( 'Footer Navigation', 'mimizuku' ),
 	] );
 }
 
@@ -28,19 +27,25 @@ function setup_wp_nav_menu( $nav_menu, $args ) {
 		'menu-item-has-children$1" aria-expanded="false"',
 		$nav_menu
 	);
-	if ( in_array( $args->theme_location, ['global-nav', 'footer-nav'] ) ) {
+
+	if ( 'global-nav' === $args->theme_location ) {
 		$nav_menu = preg_replace(
 			'/<ul class="sub-menu">/ms',
 			'<ul class="_c-menu__submenu">',
 			$nav_menu
 		);
-	} elseif ( 'drawer-nav' === $args->theme_location ) {
+		return $nav_menu;
+	}
+
+	if ( 'drawer-nav' === $args->theme_location ) {
 		$nav_menu = preg_replace(
 			'/<ul class="sub-menu">/ms',
 			'<div class="c-drawer__toggle"><i class="fa fa-angle-right"></i></div><ul class="c-drawer__submenu">',
 			$nav_menu
 		);
+		return $nav_menu;
 	}
+
 	return $nav_menu;
 }
 
@@ -52,25 +57,24 @@ add_filter( 'wp_nav_menu', __NAMESPACE__ . '\\setup_wp_nav_menu', 10, 2 );
  * https://developer.wordpress.org/reference/classes/walker_nav_menu/
  */
 function nav_menu_css_class( $classes, $item, $args, $depth ) {
-	if ( in_array( $args->theme_location, ['global-nav', 'footer-nav'] ) ) {
+	if ( 'global-nav' === $args->theme_location ) {
 		if ( $depth > 0 ) {
 			$classes[] = '_c-menu__subitem';
-			return $classes;
+		} else {
+			$classes[] = '_c-menu__item';
 		}
-
-		$classes[] = '_c-menu__item';
 		return $classes;
 	}
 
 	if ( 'drawer-nav' === $args->theme_location ) {
 		if ( $depth > 0 ) {
 			$classes[] = '_c-drawer__subitem';
-			return $classes;
+		} else {
+			$classes[] = '_c-drawer__item';
 		}
-
-		$classes[] = '_c-drawer__item';
 		return $classes;
 	}
+
 	return $classes;
 }
 
