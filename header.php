a34bd1cfba95a333f67bc4953033b49ff9638d97
Takashi Kitajima <inc@2inc.org>
2016-07-20 21:29:45 +0900

    Update hamburger-btn position

--- header.php
+++ header.php
@@ -1,19 +1,17 @@
 <header role="banner">
 	<div class="_c-container">
-		<h1><a href="<?php echo esc_url( home_url() ); ?>"><?php bloginfo( 'name' ) ?></a></h1>
+		<div class="_c-row">
+			<div class="_c-row__col _c-row__col--auto _c-row__col--lg-1-1">
+				<h1><a href="<?php echo esc_url( home_url() ); ?>"><?php bloginfo( 'name' ) ?></a></h1>
+			</div>
+			<?php if ( has_nav_menu( 'drawer-nav' ) ) : ?>
+			<div class="_c-row__col _c-row__col--auto _u-hidden-lg _c-row _c-row--right _c-row--middle">
+				<?php get_template_part( 'template-parts/hamburger-btn' ); ?>
+			</div>
+			<?php endif; ?>
 	</div>
 
 	<div class="_u-hidden-sm _u-hidden-md">
 		<?php get_template_part( 'template-parts/global-nav' ); ?>
 	</div>
-
-	<?php if ( has_nav_menu( 'drawer-nav' ) ) : ?>
-	<div class="_u-hidden-lg">
-		<div class="_c-drawer__btn _c-hamburger-btn">
-			<div class="_c-hamburger-btn__bar"></div>
-			<div class="_c-hamburger-btn__bar"></div>
-			<div class="_c-hamburger-btn__bar"></div>
-		</div>
-	</div>
-	<?php endif; ?>
 </header>
---
+++ template-parts/hamburger-btn.php
@@ -0,0 +1,5 @@
+<div class="_c-drawer__btn _c-hamburger-btn">
+	<div class="_c-hamburger-btn__bar"></div>
+	<div class="_c-hamburger-btn__bar"></div>
+	<div class="_c-hamburger-btn__bar"></div>
+</div>
