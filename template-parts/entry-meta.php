5bb2814c0f2a33d4cfe8cf1fa180132424fd9c87
Takashi Kitajima <inc@2inc.org>
2016-07-25 13:34:47 +0900

    Update entry-meta style #18

--- template-parts/entry-meta.php
+++ template-parts/entry-meta.php
@@ -3,7 +3,10 @@ $entry_meta = [];
 
 // Published
 $entry_meta['published'] = sprintf(
-	'<time datetime="%s">%s: %s</time>',
+	'<time datetime="%s">
+		<span class="screen-reader-text">%s</span>
+		%s
+	</time>',
 	esc_attr( get_the_date( 'c' ) ),
 	esc_html__( 'Published', 'mimizuku' ),
 	esc_html( get_the_date() )
@@ -11,25 +14,37 @@ $entry_meta['published'] = sprintf(
 
 // Author
 $entry_meta['author'] = sprintf(
-	'%s: <a href="%s">%s</a>',
+	'<span class="screen-reader-text">%s</span>
+	<i class="fa fa-user"></i>
+	<a href="%s">%s</a>',
 	esc_html__( 'Author', 'habakiri' ),
 	esc_url( get_author_posts_url( get_the_author_meta( 'ID' ) ) ),
 	esc_html( get_the_author() )
 );
 
 // Categories
-$entry_meta['categories'] = sprintf(
-	'%s: %s',
-	esc_html__( 'Categories', 'mimizuku' ),
-	get_the_category_list( ', ' )
-);
+$categories = get_the_category_list( ', ' );
+if ( $categories ) {
+	$entry_meta['categories'] = sprintf(
+		'<span class="screen-reader-text">%s</span>
+		<i class="fa fa-folder"></i>
+		%s',
+		esc_html__( 'Categories', 'mimizuku' ),
+		get_the_category_list( ', ' )
+	);
+}
 
 // Tags
-$entry_meta['tags'] = sprintf(
-	'%s: %s',
-	esc_html__( 'Tags', 'mimizuku' ),
-	get_the_tag_list( '', ', ' )
-);
+$tags = get_the_tag_list( '', ', ' );
+if ( $tags ) {
+	$entry_meta['tags'] = sprintf(
+		'<span class="screen-reader-text">%s</span>
+		<i class="fa fa-tags"></i>
+		%s',
+		esc_html__( 'Tags', 'mimizuku' ),
+		get_the_tag_list( '', ', ' )
+	);
+}
 
 $entry_meta = apply_filters( 'mimizuku_entry_meta_items', $entry_meta );
 ?>
