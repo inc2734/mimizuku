7a94c7b6096788675c713c0c5d3fa150d71d2773
Takashi Kitajima <inc@2inc.org>
2016-07-25 15:01:33 +0900

    Default widgets styling

--- sidebar.php
+++ sidebar.php
@@ -1,7 +1,7 @@
 <aside role="complementary">
-	<?php
-	if ( is_active_sidebar( 'sidebar' ) ) {
-		dynamic_sidebar( 'sidebar' );
-	}
-	?>
+	<?php if ( is_active_sidebar( 'sidebar' ) ) : ?>
+	<div class="_c-sidebar-widgets">
+		<?php dynamic_sidebar( 'sidebar' ); ?>
+	</div>
+	<?php endif; ?>
 </aside>
