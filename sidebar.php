d2feaba6a61df06284eadfcb23252e777f3af995
Takashi Kitajima <inc@2inc.org>
2016-07-28 14:24:42 +0900

    Fix _p-sidebar-widgets #22

--- sidebar.php
+++ sidebar.php
@@ -1,6 +1,6 @@
 <aside role="complementary">
 	<?php if ( is_active_sidebar( 'sidebar' ) ) : ?>
-	<div class="_c-sidebar-widgets">
+	<div class="_p-sidebar-widgets">
 		<?php dynamic_sidebar( 'sidebar' ); ?>
 	</div>
 	<?php endif; ?>
