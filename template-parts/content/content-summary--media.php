8975921153b54e520296d664155eb1d6ccddefee
Takashi Kitajima <inc@2inc.org>
2016-07-28 14:17:02 +0900

    Fix _p-entry-summary #22

--- template-parts/content/content-summary--media.php
+++ template-parts/content/content-summary--media.php
@@ -1,15 +1,15 @@
-<section class="_c-entry-summary _c-entry-summary--media">
+<section class="_p-entry-summary _p-entry-summary--media">
 	<div class="_c-flex-media">
 		<?php $thumbnail_url = wp_get_attachment_image_url( get_post_thumbnail_id(), 'medium' ); ?>
-		<div class="_c-entry-summary__figure _c-flex-media__figure"
+		<div class="_p-entry-summary__figure _c-flex-media__figure"
 			<?php if ( $thumbnail_url ) : ?>
 			style="background-image: url( <?php echo esc_url( $thumbnail_url ); ?> );"
 			<?php endif; ?>
 		></div>
 		<div class="_c-flex-media__body">
-			<h1 class="_c-entry-summary__title"><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></h1>
+			<h1 class="_p-entry-summary__title"><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></h1>
 			<?php get_template_part( 'template-parts/entry-meta' ); ?>
-			<div class="_c-entry-summary__content">
+			<div class="_p-entry-summary__content">
 				<?php the_excerpt(); ?>
 			</div>
 		</div>
--- template-parts/content/content-summary.php
+++ template-parts/content/content-summary.php
@@ -1,7 +1,7 @@
-<section class="_c-entry-summary">
-	<h1 class="_c-entry-summary__title"><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></h1>
+<section class="_p-entry-summary">
+	<h1 class="_p-entry-summary__title"><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></h1>
 	<?php get_template_part( 'template-parts/entry-meta' ); ?>
-	<div class="_c-entry-summary__content">
+	<div class="_p-entry-summary__content">
 		<?php the_excerpt(); ?>
 	</div>
 </section>
