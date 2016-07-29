3a454a8d04f0e3aa5933d607c96f5107277ee03f
Takashi Kitajima <inc@2inc.org>
2016-07-29 15:01:25 +0900

    Add filter hook mimizuku_entries_item_block_link

--- template-tags/get-entries-item-template.php
+++ template-tags/get-entries-item-template.php
@@ -7,9 +7,21 @@ namespace Mimizuku\Tags;
  * @return void
  */
 function get_entries_item_template() {
-	$slug = 'summary';
-	get_template_part(
-		'template-parts/content/content',
-		apply_filters( 'mimizuku_entries_item_template', $slug )
-	);
+	$is_block_link = apply_filters( 'mimizuku_entries_item_block_link', false );
+	$slug = apply_filters( 'mimizuku_entries_item_template', 'summary' );
+
+	if ( ! $is_block_link ) {
+		get_template_part( 'template-parts/content/content', $slug );
+	} else {
+		ob_start();
+		get_template_part( 'template-parts/content/content', $slug );
+		$content = ob_get_clean();
+		$allowed_html = wp_kses_allowed_html( 'post' );
+		unset( $allowed_html['a'] );
+		?>
+		<a href="<?php the_permalink(); ?>">
+			<?php echo wp_kses( $content, $allowed_html ); ?>
+		</a>
+		<?php
+	}
 }
