ca1845c4d0bacd078f20c23e161e870222e3b1e2
Takashi Kitajima <inc@2inc.org>
2016-09-10 01:50:13 +0900

    Fix footer-1row

--- template-parts/footer/footer-1row.php
+++ template-parts/footer/footer-1row.php
@@ -4,7 +4,7 @@
 			<div class="_c-row__col _c-row__col--1-1 _c-row__col--md-auto">
 				<?php get_template_part( 'template-parts/footer-nav' ); ?>
 			</div>
-			<div class="_c-row__col _c-row__col--1-1 _c-row__col--md-auto _c-row _c-row--center _c-row--md-right">
+			<div class="_c-row__col _c-row__col--1-1 _c-row__col--md-fit">
 				<?php get_template_part( 'template-parts/copyright' ); ?>
 			</div>
 		</div>
