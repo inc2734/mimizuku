5f974f2ccf5719e4008383c0294ae8981896f40b
Takashi Kitajima <inc@2inc.org>
2016-09-10 22:30:25 +0900

    Changed template-tags directory position

---
+++ functions/template-tags/loader.php
@@ -0,0 +1,7 @@
+<?php
+get_template_part( 'functions/template-tags/get-entries-item-template' );
+get_template_part( 'functions/template-tags/has-sidebar' );
+get_template_part( 'functions/template-tags/header-class' );
+get_template_part( 'functions/template-tags/is-supported-ie9' );
+get_template_part( 'functions/template-tags/the-comments' );
+get_template_part( 'functions/template-tags/the-pings' );
--- template-tags/loader.php
+++
@@ -1,7 +0,0 @@
-<?php
-get_template_part( 'template-tags/get-entries-item-template' );
-get_template_part( 'template-tags/has-sidebar' );
-get_template_part( 'template-tags/header-class' );
-get_template_part( 'template-tags/is-supported-ie9' );
-get_template_part( 'template-tags/the-comments' );
-get_template_part( 'template-tags/the-pings' );
