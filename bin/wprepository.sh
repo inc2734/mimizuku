a76e01d137452fca88ac503103af714e64aee4c8
Takashi Kitajima <inc@2inc.org>
2016-09-26 21:53:13 +0900

    Add travis build and tests script #8

---
+++ bin/wprepository.sh
@@ -0,0 +1,19 @@
+#!/usr/bin/env bash
+
+set -e
+
+if [[ "false" != "$TRAVIS_PULL_REQUEST" ]]; then
+  echo "Not deploying pull requests."
+  exit
+fi
+
+if [[ "master" != "$TRAVIS_BRANCH" ]]; then
+  echo "Not on the 'master' branch."
+  exit
+fi
+
+cd wprepository
+
+git add -A
+git commit -m "[ci skip] wprepository branch update from travis $TRAVIS_COMMIT"
+git push --quiet "https://${GH_TOKEN}@github.com/${TRAVIS_REPO_SLUG}.git" wprepository 2> /dev/null
