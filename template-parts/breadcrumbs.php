fe777d136a70d70d43b1b0dad117105a325ba78b
Takashi Kitajima <inc@2inc.org>
2016-10-23 01:10:14 +0900

    Fix breadcrumbs PHPMD error

--- template-parts/breadcrumbs.php
+++ template-parts/breadcrumbs.php
@@ -6,15 +6,15 @@
  */
 
 $breadcrumbs = new \Mimizuku\App\Models\Breadcrumbs\Breadcrumbs();
-$items = $breadcrumbs->get();
+$items = array_values( $breadcrumbs->get() );
 ?>
 <ol itemscope itemtype="http://schema.org/BreadcrumbList">
-	<?php $i = 0; foreach ( $items as $item ) : $i ++; ?>
+	<?php foreach ( $items as $key => $item ) : ?>
 	<li itemprop="itemListElement" itemscope itemtype="http://schema.org/ListItem">
 		<a itemscope itemtype="http://schema.org/Thing" itemprop="item" href="<?php echo esc_url( $item['link'] ); ?>">
 			<span itemprop="name"><?php echo esc_html( $item['title'] ); ?></span>
 		</a>
-		<meta itemprop="position" content="<?php echo esc_attr( $i ); ?>" />
+		<meta itemprop="position" content="<?php echo esc_attr( $key + 1 ); ?>" />
 	</li>
 	<?php endforeach; ?>
 </ol>
