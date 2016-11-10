5305b36c4ee859cac124332d60af4b990114b622
Takashi Kitajima <inc@2inc.org>
2016-11-10 22:37:40 +0900

    Update packages

--- app/tags/header-class.php
+++ app/tags/header-class.php
@@ -22,7 +22,7 @@ function header_class( $add_classes = [] ) {
 	}
 
 	$is_sticky_header = apply_filters( 'mimizuku_is_sticky_header', false );
-	if ( $is_overlay_header && $is_sticky_header ) {
+	if ( $is_sticky_header ) {
 		$classes[] = '_l-header--sticky';
 	}
 
