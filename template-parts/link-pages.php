c7f7d12e6ceee9b62d103c7d3c40c513a3d825c8
Takashi Kitajima <inc@2inc.org>
2017-03-30 12:35:22 +0900

    Refactoring pagination structure

--- template-parts/link-pages.php
+++ template-parts/link-pages.php
@@ -5,15 +5,25 @@
  * @license GPL-2.0+
  */
 
-add_filter( 'wp_link_pages_link', function( $link ) {
-	$link = preg_replace( '/^(\d+)$/', '<span class="page-numbers current">$1</span>', $link );
-	$link = preg_replace( '/^<a([^>]+)>(\d+?)<\/a>$/', '<a class="page-numbers" $1>$2</a>', $link );
-	$link .= "\n";
-	return $link;
+add_filter( 'wp_link_pages_link', function( $pagination ) {
+	$pagination = preg_replace(
+		'/^(\d+)$/',
+		'<span class="page-numbers current">$1</span>',
+		$pagination
+	);
+	$pagination = preg_replace(
+		'/^<a([^>]+)>(\d+?)<\/a>$/',
+		'<a class="page-numbers" $1>$2</a>',
+		$pagination
+	);
+	$pagination = \Mimizuku\App\Models\Pagination::replace_basis_pager_structure( $pagination );
+	$pagination .= "\n";
+
+	return $pagination;
 } );
 
 wp_link_pages( [
-	'before'           => '<div class="_p-pagination"><h2 class="screen-reader-text">' . esc_html__( 'Posts navigation', 'mimizuku' ) . '</h2><div class="nav-links">',
+	'before'           => '<div class="_c-pagination"><h2 class="screen-reader-text">' . esc_html__( 'Posts navigation', 'mimizuku' ) . '</h2><div class="nav-links">',
 	'after'            => '</div></div>',
 	'link_before'      => '',
 	'link_after'       => '',
