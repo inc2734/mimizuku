446e93378bf33c01c925da3a7f7241620b59db5a
Takashi Kitajima <inc@2inc.org>
2017-06-06 14:38:15 +0900

    Fix #43

--- app/bin/theme-unit-test-ja.sh
+++ app/bin/theme-unit-test-ja.sh
@@ -13,17 +13,27 @@ datetime=`date +%Y%m%d%H%M%S`
 wp db export "$themedir/../../dump-$datetime.sql"
 
 if [ -e "$themedir/../../dump-$datetime.sql" ]; then
-	wp plugin install --activate wordpress-importer
-	wp plugin is-installed wordpress-importer
+  wp plugin is-installed wordpress-importer
 
-	if [ $? -eq 0 ]; then
-		wp menu delete $(wp menu list --fields=term_id) --quiet
-		wp post delete $(wp post list --post_type=page,post --posts_per_page=-1 --format=ids) --force --quiet
+  if [ $? -ne 0 ]; then
+    wp plugin install --activate wordpress-importer
+  fi
 
-		wget https://raw.githubusercontent.com/jawordpressorg/theme-test-data-ja/master/wordpress-theme-test-date-ja.xml -P $themedir
-		wp import "$themedir/wordpress-theme-test-date-ja.xml" --authors=create --quiet
-		wp menu location assign "All Pages" global-nav
-		wp menu location assign "All Pages" drawer-nav
-		rm -rf "$themedir/wordpress-theme-test-date-ja.xml"
-	fi
+  if [ $? -eq 0 ]; then
+    wp menu list --format=ids
+    if [ $? -ne 0 ]; then
+      wp menu delete $?
+    fi
+
+    wp post list --post_type=page,post --posts_per_page=-1 --format=ids
+    if [ $? -ne 0 ]; then
+      wp post delete $?
+    fi
+
+    wget https://raw.githubusercontent.com/jawordpressorg/theme-test-data-ja/master/wordpress-theme-test-date-ja.xml -P $themedir
+    wp import "$themedir/wordpress-theme-test-date-ja.xml" --authors=create --quiet
+    wp menu location assign "All Pages" global-nav
+    wp menu location assign "All Pages" drawer-nav
+    rm -rf "$themedir/wordpress-theme-test-date-ja.xml"
+  fi
 fi
