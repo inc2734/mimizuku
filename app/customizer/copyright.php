e3646af01a119d6e3a01f7a22148aca0f3ae34bc
Takashi Kitajima <inc@2inc.org>
2017-07-22 14:48:21 +0900

    Add copyright customizer

---
+++ app/customizer/copyright.php
@@ -0,0 +1,37 @@
+<?php
+/**
+ * @package mimizuku
+ * @author inc2734
+ * @license GPL-2.0+
+ */
+
+
+$theme_link = sprintf(
+	'<a href="https://2inc.org" target="_blank">%s</a>',
+	__( 'Monkey Wrench', 'mimizuku' )
+);
+
+$wordpress_link = sprintf(
+	'<a href="https://wordpress.org/" target="_blank">%s</a>',
+	__( 'WordPress', 'mimizuku' )
+);
+
+$theme_by   = sprintf( __( 'mimizuku theme by %s', 'mimizuku' ), $theme_link );
+$powered_by = sprintf( __( 'Powered by %s', 'mimizuku' ), $wordpress_link );
+$copyright  = $theme_by . ' ' . $powered_by;
+
+$customizer = Inc2734_WP_Customizer_Framework::init();
+
+$customizer->section( 'copyright', [
+	'title' => __( 'Copyright', 'mimizuku' ),
+] );
+
+$customizer->control( 'text', 'copyright', [
+	'label'       => __( 'Copyright', 'mimizuku' ),
+	'description' => __( 'HTML usable', 'mimizuku' ),
+	'default'     => $copyright,
+] );
+
+$section = $customizer->get_section( 'copyright' );
+$control = $customizer->get_control( 'copyright' );
+$control->join( $section );
--- template-parts/copyright.php
+++ template-parts/copyright.php
@@ -4,27 +4,7 @@
  * @author inc2734
  * @license GPL-2.0+
  */
-
-$theme_url     = 'https://2inc.org';
-$wordpress_url = 'https://wordpress.org/';
-$theme_link = sprintf(
-	'<a href="%s" target="_blank">%s</a>',
-	esc_url( $theme_url ),
-	__( 'Monkey Wrench', 'mimizuku' )
-);
-$wordpress_link = sprintf(
-	'<a href="%s" target="_blank">%s</a>',
-	esc_url( $wordpress_url ),
-	__( 'WordPress', 'mimizuku' )
-);
-$theme_by   = sprintf( __( 'mimizuku theme by %s', 'mimizuku' ), $theme_link );
-$powered_by = sprintf( __( 'Powered by %s', 'mimizuku' ), $wordpress_link );
-$copyright  = sprintf(
-	'%s&nbsp;%s',
-	$theme_by,
-	$powered_by
-);
 ?>
 <div class="c-copyright">
-	<?php echo wp_kses_post( apply_filters( 'mimizuku_copyright', $copyright ) ); ?>
+	<?php echo wp_kses_post( apply_filters( 'mimizuku_copyright', get_theme_mod( 'copyright' ) ) ); ?>
 </div>
