be2b93d267ad5295abb4897dbb56680e37974c83
Takashi Kitajima <inc@2inc.org>
2017-07-31 15:51:12 +0900

    Some fixes

--- core/bin/scaffold.sh
+++ core/bin/scaffold.sh
@@ -5,7 +5,7 @@ set -e;
 themedir=$(pwd)
 
 if [ -z "$(ls $themedir/resources/)" ]; then
-  cp -r "$themedir/core/.resources/" "$themedir/resources/"
+  cp -r "$themedir/core/.resources/*" "$themedir/resources/"
   echo "Success ! Expanded to $themedir/resources/"
 else
   echo "$themedir/resources/* already exsist !"
