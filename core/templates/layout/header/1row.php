3b171682413f0c9f6e58bbae5e7a53e953bf4bee
Takashi Kitajima <inc@2inc.org>
2017-08-02 14:52:03 +0900

    Some fixes

--- core/templates/layout/header/1row.php
+++ core/templates/layout/header/1row.php
@@ -5,21 +5,21 @@
  * @license GPL-2.0+
  */
 ?>
-<header class="l-header l-header--<?php echo get_theme_mod( 'header-layout' ); ?>" role="banner">
+<header class="l-header l-header--<?php echo esc_attr( get_theme_mod( 'header-layout' ) ); ?>" role="banner">
 	<div class="c-container">
-		<div class="l-<?php echo get_theme_mod( 'header-layout' ); ?>-header">
-			<div class="l-<?php echo get_theme_mod( 'header-layout' ); ?>-header__branding">
+		<div class="l-<?php echo esc_attr( get_theme_mod( 'header-layout' ) ); ?>-header">
+			<div class="l-<?php echo esc_attr( get_theme_mod( 'header-layout' ) ); ?>-header__branding">
 				<?php get_template_part( '../core/template-parts/site-branding' ); ?>
 			</div>
 
 			<?php if ( has_nav_menu( 'global-nav' ) ) : ?>
-				<div class="l-<?php echo get_theme_mod( 'header-layout' ); ?>-header__for-wide">
+				<div class="l-<?php echo esc_attr( get_theme_mod( 'header-layout' ) ); ?>-header__for-wide">
 					<?php get_template_part( '../core/template-parts/global-nav' ); ?>
 				</div>
 			<?php endif; ?>
 
 			<?php if ( has_nav_menu( 'drawer-nav' ) ) : ?>
-				<div class="l-<?php echo get_theme_mod( 'header-layout' ); ?>-header__for-narrow">
+				<div class="l-<?php echo esc_attr( get_theme_mod( 'header-layout' ) ); ?>-header__for-narrow">
 					<?php get_template_part( '../core/template-parts/hamburger-btn' ); ?>
 				</div>
 			<?php endif; ?>
--- core/templates/layout/header/center.php
+++ core/templates/layout/header/center.php
@@ -5,27 +5,27 @@
  * @license GPL-2.0+
  */
 ?>
-<header class="l-header l-header--<?php echo get_theme_mod( 'header-layout' ); ?>" role="banner">
-	<div class="l-<?php echo get_theme_mod( 'header-layout' ); ?>-header">
-		<div class="l-<?php echo get_theme_mod( 'header-layout' ); ?>-header__row">
+<header class="l-header l-header--<?php echo esc_attr( get_theme_mod( 'header-layout' ) ); ?>" role="banner">
+	<div class="l-<?php echo esc_attr( get_theme_mod( 'header-layout' ) ); ?>-header">
+		<div class="l-<?php echo esc_attr( get_theme_mod( 'header-layout' ) ); ?>-header__row">
 			<?php if ( has_nav_menu( 'drawer-nav' ) ) : ?>
-				<div class="l-<?php echo get_theme_mod( 'header-layout' ); ?>-header__for-narrow"></div>
+				<div class="l-<?php echo esc_attr( get_theme_mod( 'header-layout' ) ); ?>-header__for-narrow"></div>
 			<?php endif; ?>
 
-			<div class="l-<?php echo get_theme_mod( 'header-layout' ); ?>-header__branding">
+			<div class="l-<?php echo esc_attr( get_theme_mod( 'header-layout' ) ); ?>-header__branding">
 				<?php get_template_part( '../core/template-parts/site-branding' ); ?>
 			</div>
 
 			<?php if ( has_nav_menu( 'drawer-nav' ) ) : ?>
-				<div class="l-<?php echo get_theme_mod( 'header-layout' ); ?>-header__for-narrow">
+				<div class="l-<?php echo esc_attr( get_theme_mod( 'header-layout' ) ); ?>-header__for-narrow">
 					<?php get_template_part( '../core/template-parts/hamburger-btn' ); ?>
 				</div>
 			<?php endif; ?>
 		</div>
 
 		<?php if ( has_nav_menu( 'global-nav' ) ) : ?>
-			<div class="l-<?php echo get_theme_mod( 'header-layout' ); ?>-header__row">
-				<div class="l-<?php echo get_theme_mod( 'header-layout' ); ?>-header__for-wide">
+			<div class="l-<?php echo esc_attr( get_theme_mod( 'header-layout' ) ); ?>-header__row">
+				<div class="l-<?php echo esc_attr( get_theme_mod( 'header-layout' ) ); ?>-header__for-wide">
 					<?php get_template_part( '../core/template-parts/global-nav' ); ?>
 				</div>
 			</div>
