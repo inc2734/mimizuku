8580a3b8b2ee92680ec08517bcf6e7cc4db5a43c
Takashi Kitajima <inc@2inc.org>
2017-08-11 21:41:40 +0900

    Some fixes

--- core/bin/create-release-branch.sh
+++ core/bin/create-release-branch.sh
@@ -29,4 +29,7 @@ ls -la
 yarn install
 composer install --no-dev
 rm -rf composer.json composer.lock package.json yarn.lock gulpfile.js node_modules .gitignore
-cd ../
+
+git add -A
+git commit -m "[ci skip] release branch update from travis $TRAVIS_COMMIT"
+git push origin release
