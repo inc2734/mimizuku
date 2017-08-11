d834434a63f1ccc0bd66a0b72fc942000c85ad77
Takashi Kitajima <inc@2inc.org>
2017-08-11 19:28:01 +0900

    Refactoring like-me-box

--- resources/template-parts/like-me-box.php
+++ resources/template-parts/like-me-box.php
@@ -15,20 +15,4 @@ if ( ! $facebook_page_name ) {
 	return;
 }
 
-add_filter( 'inc2734_wp_like_me_box_thumbnail', function( $thumbnail ) {
-	if ( $thumbnail ) {
-		return $thumbnail;
-	}
-
-	if ( get_site_icon_url() ) {
-		return get_site_icon_url();
-	}
-
-	if ( get_theme_mod( 'default-og-image' ) ) {
-		return get_theme_mod( 'default-og-image' );
-	}
-
-	return $thumbnail;
-} );
-
 echo do_shortcode( '[wp_like_me_box facebook_page_name="' . $facebook_page_name . '"]' );
